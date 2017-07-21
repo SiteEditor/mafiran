d8d106fa9a3dacfa2585212b7387641407a72021
parsaatef <parsaatef@gmail.com>
2017-07-21 11:04:18 +0430

    call Masonry Script

--- functions.php
+++ functions.php
@@ -19,9 +19,11 @@ function mafiran_enqueue_styles() {
 
     wp_enqueue_script( "mafiran-balls-script" , get_stylesheet_directory_uri() . '/assets/js/balls.js' , array( 'jquery', 'sed-livequery' ) , "1.0.0" , true );
 
-    //wp_enqueue_script('jquery-scrollbar');
+    wp_enqueue_script('sed-masonry');
 
-    //wp_enqueue_style('custom-scrollbar');
+    wp_enqueue_script('jquery-scrollbar');
+
+    wp_enqueue_style('custom-scrollbar');
 
     wp_enqueue_style("carousel");
 
